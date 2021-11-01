effd3fd0c77c80492a9afdd31abfad636e55cdfd
Onii-chan <failer007@live.dk>
2021-11-01 10:16:30 +0100

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hooray for Continous Integration!");
+            Console.WriteLine("Hooray for Continous Integration.");
         }
     }
 }
